92a0f644ed3b7171cba56153d03f28ed88631eef
nicbongo <nicbongo@protonmail.com>
2020-01-24 14:38:05 -0400

    cleaning up functions

--- smurfs/src/components/NewSmurf.js
+++ smurfs/src/components/NewSmurf.js
@@ -4,31 +4,46 @@ import { newSmurf } from "../actions"
 
 const NewSmurf = () => {
 
-  const [name, setName] = useState("")
-  const [age, setAge] = useState() 
-  const [height, setHeight] = useState()
+  const [smurfForm, setSmurfForm] = useState({
+    name: "",
+    age: null,
+    height: null
+  })
+
+  const handleChanges = e => {
+    setSmurfForm({
+      ...smurfForm,
+      [e.target.name]: e.target.value
+    })
+  }
   
+  const onSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3333/smurfs", smurfForm)
+  }
 
   return (
-    <form onSubmit={ e => { 
-      e.preventDefault();
-      axios.post("http://localhost:3333/smurfs", {name, age, height})}}>
+    <form onSubmit={onSubmit}>
       
       <input 
         type="text"
+        name="name"
         placeholder="what's their name?"
-        onChange={e => setName(e.target.value)}
+        onChange={handleChanges}
       />
       <input 
         type="number"
+        name="age"
         placeholder="how many years old?" 
-        onChange={e => setAge(e.target.value)}
+        onChange={handleChanges}
 
       />
       <input 
         type="number"
+        name="height"
         placeholder="how tall?" 
-        onChange={e => setHeight(e.target.value)}
+        onChange={handleChanges}
 
       />
       <button type="submit">Join the village!</button>

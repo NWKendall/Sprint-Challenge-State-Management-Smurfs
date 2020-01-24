9bbfc813813c9fad3c3e8c412b771ec1b0d96c5c
nicbongo <nicbongo@protonmail.com>
2020-01-24 12:37:23 -0400

    mapped state to props

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import axios from "axios"
+import { connect } from "react-redux"
+
+const SmurfList = props => {
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      console.log(`this is from useEffect`, res)
+    })
+    .catch(err => {
+      console.log(`this is an error from useEffect`, err)
+    })
+  }, [])
+
+
+  return (
+    <div>
+      asdasd
+      
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  
+  return {
+    isLoading: state.isLoading,
+    error: state.error,
+    smurfs: [...state.smurfs]
+  }
+}
+
+
+export default connect (mapStateToProps, {})(SmurfList)
---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isLoading: false,
+  error: "",
+  smurfs: []
+};
+
+export const RootReducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
